8f41db017562f46a1671117214d63e73ecac4a18
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-11-09 16:52:35 +1100

    Seeds for Users and Options

---
+++ database/seeds/OptionsTableSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+
+class OptionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Add default config.
+        DB::table('options')->insert([
+            [
+                'option_nice_name' => 'Site Admin Email',
+                'option_name' => 'global_email',
+                'option_value' => 'admin@site.com',
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ]);
+    }
+}
