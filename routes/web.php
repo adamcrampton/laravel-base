538e057065347370fb65277fcf9224f71880ec26
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-11-09 16:46:58 +1100

    Added Option MVC. Updted some routes

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,12 @@
 <?php
 
 // Front end routes.
-Route::resource('/', 'DisplayController');
+Route::resource('/', 'PageController');
+
+// Admin routes.
+Route::resource('manage', 'ManageController');
+Route::resource('options', 'OptionController');
+
+// Auth routes.
+Auth::routes();
+Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
