95088906f1e663bf6b02714540e844f519e1a96f
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-11-21 07:12:17 +1100

    WIP hooking up loop + page + option

--- app/Http/Controllers/LoopController.php
+++ app/Http/Controllers/LoopController.php
@@ -8,12 +8,22 @@ use App\Models\Option;
 
 class LoopController extends Controller
 {
+	private $globalOptions;
+    public $pageData;
+
+    public function __construct(Page $page, Option $option)
+    {
+        // Get default options and active page data.
+        $this->globalOptions = $option->getGlobalConfig();
+        $this->pageData = $page->getPages($this->globalOptions['post_loop_limit']);
+    }
+
     /**
      * Display a listing of pages for loop view.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Page $page)
     {
         // Return public home page.
         return view('loop.index', [
--- app/Models/Option.php
+++ app/Models/Option.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Option extends Model
 {
-    //
+	public functon getGlobalConfig() {
+		// TODO: Fetch global options.
+    	return ['post_loop_limit' => 5];	
+	}
+    
 }
