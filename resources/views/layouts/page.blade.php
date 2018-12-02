1dab37dfc310a16c798bd1aa53339001c1aa794c
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-12-03 07:05:46 +1100

    Added single page layout

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -1,92 +1,37 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Vendor CSS -->
-    <link rel="stylesheet" href="{{ URL::asset('vendor/bootstrap/css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('vendor/venobox/css/venobox.css') }}">
-    
-    <!-- App CSS -->
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
-    <link rel="stylesheet" href="{{ URL::asset('css/manage.css') }}">
-
-    <!-- Vendor Scripts -->
-    <script src="{{ URL::asset('js/manifest.js') }}"></script>
-    <script src="{{ URL::asset('js/vendor.js') }}"></script>
-
-    <title>@yield('title')</title>
-  </head>
+@include('includes.head')
   <body {{ isset($modelName) ? 'data-model-name='.strtolower($modelName) : '' }}>
-
-    @if ($errors->any())
-    <div class="alert alert-danger alert-top" role="alert">
-        <p>Error detail lead text:</p>
-        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @elseif (session()->has('warning'))
-    <div class="alert alert-warning alert-top alert-dismissible">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      {!! session()->get('warning') !!}
-    </div>
-    @elseif (session()->has('success'))
-    <div class="alert alert-success alert-top alert-dismissible">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-        <span aria-hidden="true">&times;</span>
-      </button>
-      {!! session()->get('success') !!}
-    </div>
-    @endif
-    <!-- Navigation -->
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark static-top">
-      <div class="container">
-        <a class="navbar-brand" href="/">Laravel</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item">
-                <a class="nav-link {{ Request::is('manage') ? 'active' : '' }}" href="/manage">Home
-                  <span class="sr-only">(current)</span>
-                </a>
-              </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  {{--  Errors and alerts --}}
+  @include('includes.alerts')
+    {{-- Navigation --}}
+    @include('includes.nav')
+    {{-- Page Content --}}
     <div class="container">
-    @if (session('status'))
-      <div class="card-body">
-        <div class="alert alert-success" role="alert">
-        {{ session('status') }}
+      <div class="row">
+        <!-- Page Content Column -->
+        <div class="col-lg-8">
+          <!-- Title -->
+          <h1 class="mt-4">Post Title</h1>
+          <!-- Author -->
+          <p class="lead">
+            by
+            <a href="#">{{ $pageValues->user->name }}</a>
+          </p>
+          <hr>
+          <!-- Date/Time -->
+          <p>{{ $pageValues->updated_at }}</p>
+          <hr>
+          <!-- Lead Image -->
+          <img class="img-fluid rounded" src="{{ $pageValues->image_path }}" alt="">
+          <hr>
+          <!-- Page Content -->
+          <p class="lead">{{ $pageValues->content }}</p>
+          <hr>
         </div>
+        {{-- Sidebar --}}
+        @include('includes.sidebar')
       </div>
+      <!-- /.row -->
     </div>
-    @endif
-    <div class="container container__main-container">
-      <!-- Page Content -->
-      <div class="col-lg-12 clearfix">
-        <div class="mt-5 float-md-left">
-        <h1>{{ isset($pageTitle) ? $pageTitle : 'Untitled' }}</h1>
-        </div>
-      </div>
-      @yield('content')
-    </div> 
-    
-    <!-- App Scripts -->
-    <script src="{{ URL::asset('js/app.js') }}"></script>
-    <script src="{{ URL::asset('js/page.js') }}"></script>
-  </body>
-</html>
+    <!-- /.container -->
+    {{-- Footer --}}
+  @include('includes.footer')
